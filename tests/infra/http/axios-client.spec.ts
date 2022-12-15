f0edef309a35cf298fb8e37b36aa7c1395d3f261
pedrocarr <eng.pedro.amb@gmail.com>
2022-12-15 10:30:37 -0300

    isolate SUT creation

---
+++ tests/infra/http/axios-client.spec.ts
@@ -0,0 +1,36 @@
+import { HttpGetClient } from '@/infra/http'
+
+import axios from 'axios'
+
+jest.mock('axios')
+
+class AxiosHttpClient {
+  async get (args: HttpGetClient.Params): Promise<void> {
+    await axios.get(args.url, { params: args.params })
+  }
+}
+
+describe('AxiosHttpClient', () => {
+  let sut: AxiosHttpClient
+  let url: string
+  let fakeAxios: jest.Mocked<typeof axios>
+  let params: object
+
+  beforeAll(() => {
+    url = 'any_url'
+    params = { any: 'any' }
+    fakeAxios = axios as jest.Mocked<typeof axios>
+  })
+
+  beforeEach(() => {
+    sut = new AxiosHttpClient()
+  })
+  describe('get', () => {
+    it('should call get with correct params', async () => {
+      await sut.get({ url, params })
+
+      expect(fakeAxios.get).toHaveBeenCalledWith(url, { params })
+      expect(fakeAxios.get).toHaveBeenCalledTimes(1)
+    })
+  })
+})
